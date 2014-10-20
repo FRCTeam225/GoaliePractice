680023c5c20a5288ba9c12f653ec4cd99cdb47ba
TheSkyHighPolishGuy <awolnikowski@gmail.com>
2014-10-19 21:01:26 -0400

    Removed Unecessary Interior Repos

---
+++ src/edu/wpi/first/wpilibj/templates/autonomous/OneBallHot.java
@@ -0,0 +1,21 @@
+package edu.wpi.first.wpilibj.templates.autonomous;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
+import edu.wpi.first.wpilibj.templates.commands.Spit;
+import edu.wpi.first.wpilibj.templates.commands.DriveDistance;
+import edu.wpi.first.wpilibj.templates.commands.Stow;
+
+/**
+ *
+ * @author Andrew
+ */
+public class OneBall extends CommandGroup {
+    public OneBall()
+    {
+        addSequential(new Stow());
+        addSequential(new DriveDistance(10000));
+        addSequential(new Spit());
+        addSequential(new WaitCommand(2.0));
+    }
+}
